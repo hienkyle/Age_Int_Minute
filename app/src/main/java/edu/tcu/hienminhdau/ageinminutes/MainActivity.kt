c4b1bace47fd766ce85c7a37f9e4be23aec12844
hienkyle <hien2003.dm@gmail.com>
2024-09-19 14:51:57 -0500

    comments added to MainActivity.kt

--- app/src/main/java/edu/tcu/hienminhdau/ageinminutes/MainActivity.kt
+++ app/src/main/java/edu/tcu/hienminhdau/ageinminutes/MainActivity.kt
@@ -30,23 +30,32 @@ class MainActivity : AppCompatActivity() {
         val currentYear = calendar.get(Calendar.YEAR)
         val currentMonth = calendar.get(Calendar.MONTH)
         val currentDay = calendar.get(Calendar.DATE)
+
+        // calculate the current time in minute
         val currentDateInMs = calendar.timeInMillis
         val  currentDateInMin = currentDateInMs / 60000
 
         val dialog = DatePickerDialog(this, {
             _, selectedYear, selectedMonth, selectedDay ->
             val selectedDateTv: TextView = findViewById(R.id.selectedDateTv)
+            // craft the text to display the selected text
             val selectedText = "${selectedMonth + 1}/$selectedDay/$selectedYear"
             selectedDateTv.text = selectedText
 
+            // calculate the time in minute of the selected date
             calendar.set(selectedYear, selectedMonth, selectedDay)
             val setDateInMs = calendar.timeInMillis
             val setDateInMin = setDateInMs / 60000
 
+            // calculate the difference between the two timestamps
             val minutesTv: TextView = findViewById(R.id.minutesTv)
             minutesTv.text = "${currentDateInMin - setDateInMin}"
         }, currentYear, currentMonth, currentDay)
+
+        // set the maximum date that can be selected
         dialog.datePicker.maxDate = currentDateInMs
+
+        // show the dialog
         dialog.show()
     }
 }
